a33779ed031b24a8fa52d45f47a24085491f5876
PLuIIIkin <viazovskih@rambler.ru>
2023-01-10 07:12:02 +0800

    portfolio

---
+++ dist/js/script.js
@@ -0,0 +1,48 @@
+const hanburger = document.querySelector('.hamburger'),
+  menu = document.querySelector('.menu'),
+  closeElem = document.querySelector('.menu__close');
+
+hanburger.addEventListener('click', () => {
+  menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+
+const dividerStyle = document.querySelectorAll('.sidepanel__divider');
+
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset >= 400) {
+    dividerStyle.forEach((el) => (el.style.backgroundColor = 'black'));
+  } else {
+    dividerStyle.forEach((el) => (el.style.backgroundColor = ''));
+  }
+});
+
+const textStyle = document.querySelectorAll('.sidepanel__text span');
+
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset >= 300) {
+    textStyle.forEach((el) => (el.style.color = 'black'));
+  } else {
+    textStyle.forEach((el) => (el.style.color = ''));
+  }
+});
+
+const linkStyle = document.querySelectorAll('.sidepanel__link svg path');
+
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset >= 500) {
+    linkStyle.forEach((el) => (el.style.fill = 'black'));
+  } else {
+    linkStyle.forEach((el) => (el.style.fill = ''));
+  }
+});
+
+const counters = document.querySelectorAll('.skills__strip_percent'),
+  lines = document.querySelectorAll('.skills__strip_scale span');
+
+counters.forEach((item, i) => {
+  lines[i].style.width = item.innerHTML;
+});
