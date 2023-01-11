5d7e2106fcce5dd709cf66a138ad5683354cd5d0
PLuIIIkin_work <viazovskih@rambler.ru>
2023-01-11 11:45:38 +0800

    podbivka

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,48 +1,55 @@
 const hanburger = document.querySelector('.hamburger'),
-  menu = document.querySelector('.menu'),
-  closeElem = document.querySelector('.menu__close');
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+const menuLinks = document.querySelectorAll('.menu__list li a');
 
 hanburger.addEventListener('click', () => {
-  menu.classList.add('active');
+    menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
-  menu.classList.remove('active');
+    menu.classList.remove('active');
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('active');
+  });
 });
 
 const dividerStyle = document.querySelectorAll('.sidepanel__divider');
 
 document.addEventListener('scroll', () => {
-  if (window.pageYOffset >= 400) {
-    dividerStyle.forEach((el) => (el.style.backgroundColor = 'black'));
-  } else {
-    dividerStyle.forEach((el) => (el.style.backgroundColor = ''));
-  }
+    if (window.pageYOffset >= 400) {
+        dividerStyle.forEach((el) => (el.style.backgroundColor = 'black'));
+    } else {
+        dividerStyle.forEach((el) => (el.style.backgroundColor = ''));
+    }
 });
 
 const textStyle = document.querySelectorAll('.sidepanel__text span');
 
 document.addEventListener('scroll', () => {
-  if (window.pageYOffset >= 300) {
-    textStyle.forEach((el) => (el.style.color = 'black'));
-  } else {
-    textStyle.forEach((el) => (el.style.color = ''));
-  }
+    if (window.pageYOffset >= 300) {
+        textStyle.forEach((el) => (el.style.color = 'black'));
+    } else {
+        textStyle.forEach((el) => (el.style.color = ''));
+    }
 });
 
 const linkStyle = document.querySelectorAll('.sidepanel__link svg path');
 
 document.addEventListener('scroll', () => {
-  if (window.pageYOffset >= 500) {
-    linkStyle.forEach((el) => (el.style.fill = 'black'));
-  } else {
-    linkStyle.forEach((el) => (el.style.fill = ''));
-  }
+    if (window.pageYOffset >= 500) {
+        linkStyle.forEach((el) => (el.style.fill = 'black'));
+    } else {
+        linkStyle.forEach((el) => (el.style.fill = ''));
+    }
 });
 
 const counters = document.querySelectorAll('.skills__strip_percent'),
-  lines = document.querySelectorAll('.skills__strip_scale span');
+    lines = document.querySelectorAll('.skills__strip_scale span');
 
 counters.forEach((item, i) => {
-  lines[i].style.width = item.innerHTML;
+    lines[i].style.width = item.innerHTML;
 });
